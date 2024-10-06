2af577ece6d860c0667340b4fa5990ff37550095
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-06 18:15:35 +0200

    Implement logic that adds entries to phone book using HTTP post requests - complete execise 3.5, step 5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     "id": "1",
@@ -23,6 +25,8 @@ let persons = [
     "number": "39-23-6423122"
   }
 ]
+
+const generateId = () => String(Math.floor(Math.random() * (200 - 1) + 1));
 
 app.get("/info", (request, response) => {
   response.setHeader('Date', new Date().toUTCString());
@@ -54,6 +58,19 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter(person => person.id !== id);
 
   response.status(404).end();
+})
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    id: generateId(),
+    number: body.number,
+  }
+
+  persons = persons.concat(person);
+  response.json(person);
 })
 
 const PORT = 3002

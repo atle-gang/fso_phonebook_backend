defcdba77f7e7de96cd0255bb67be4daf64f45e9
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-06 18:29:17 +0200

    Implement error handling for new entries with no name, number or name that already exists - complete exercise 3.6, step 6

--- index.js
+++ index.js
@@ -63,6 +63,20 @@ app.delete("/api/persons/:id", (request, response) => {
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number is missing",
+    })
+  }
+
+  const nameExists = persons.some(person => person.name === body.name);
+
+  if (nameExists) {
+    return response.status(400).json({
+      error: 'name must be unique',
+    })
+  }
+
   const person = {
     name: body.name,
     id: generateId(),

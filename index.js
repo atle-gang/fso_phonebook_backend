1fff66931af747dc96a1ee2f241137960557e3a9
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-08 21:13:38 +0200

    Refactor 'index.js' and add logic to fetch phone book entries from the databasse

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
 const express = require("express");
 const app = express();
 
-const morgan = require("morgan");
+require('dotenv').config();
 
+const Person = require("./models/person");
+
+const morgan = require("morgan");
 const cors = require("cors");
 
 app.use(express.static('dist'))
 app.use(cors())
 
 let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122"
-  }
-];
-
+]
 
 app.use(express.json());
 
@@ -54,9 +36,36 @@ app.get("/info", (request, response) => {
   );
 });
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons);
+  });
 });
+
+app.get("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    });
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson);
+  })
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  Person.findById(request.params.id).then(person => {
+    response.json(person);
+  })
+})
 
 app.get("/api/persons/:id", (request, response) => {
   const id = request.params.id;

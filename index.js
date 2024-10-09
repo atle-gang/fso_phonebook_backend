022fa56a5bebc7377d0be765786079da41f2ea2b
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-09 10:32:59 +0200

    Refactor delete function so that deleting entries is reflected in the db

--- index.js
+++ index.js
@@ -6,32 +6,32 @@ const Person = require("./models/person");
 
 const morgan = require("morgan");
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);
-})
+});
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
-  if (error.name === "CastError") return response.status(400).send({ error: "malformatted id"});
+  if (error.name === "CastError") return response.status(400).send({ error: "malformatted id" });
 
   next(error);
-}
+};
 
 const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
-)
+);
 
 const unknownEndPoint = (request, response) => {
-  response.status(404).send({ error: "Unknown endpoint"});
-}
+  response.status(404).send({ error: "Unknown endpoint" });
+};
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -51,25 +51,27 @@ app.get("/api/persons", (request, response) => {
   const person = new Person({
     name: body.name,
     number: body.number
-  })
+  });
 
   person.save().then(savedPerson => {
     response.json(savedPerson);
-  })
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
   Person.findById(request.params.id).then(person => {
     response.json(person);
-  })
-})
-
+  });
+});
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(404).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => {
+      console.error("Error deleting entry:", error);
+    });
 });
 
 app.post("/api/persons", (request, response) => {
@@ -102,7 +104,7 @@ app.post("/api/persons", (request, response) => {
 app.use(unknownEndPoint);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});

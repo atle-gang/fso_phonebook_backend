3c177a60d191883a49814be2e17e7f6e1844e954
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-09 10:26:45 +0200

    Order middleware loading

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
 const express = require("express");
 const app = express();
-
 require('dotenv').config();
 
 const Person = require("./models/person");
 
 const morgan = require("morgan");
-const cors = require("cors");
 
 app.use(express.static('dist'))
-app.use(cors())
-
-let persons = [
-]
-
-app.use(express.json());
 
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);
 })
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") return response.status(400).send({ error: "malformatted id"});
+
+  next(error);
+}
+
+const cors = require("cors");
+
+app.use(cors())
+app.use(express.json());
+
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
 
-const generateId = () => String(Math.floor(Math.random() * (200 - 1) + 1));
-
-app.get("/info", (request, response) => {
-  response.setHeader("Date", new Date().toUTCString());
-  const responseDate = response.getHeaders().date;
-  response.send(
-    `<p>Phone book has info of ${persons.length} people</p>
-    <p>${responseDate}</p>
-    `
-  );
-});
+const unknownEndPoint = (request, response) => {
+  response.status(404).send({ error: "Unknown endpoint"});
+}
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -67,17 +64,6 @@ app.get("/api/persons/:id", (request, response) => {
   })
 })
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.statusMessage = `Person with id ${id} not found`;
-    response.status(404).end();
-  }
-});
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
@@ -112,6 +98,9 @@ app.post("/api/persons", (request, response) => {
   persons = persons.concat(person);
   response.json(person);
 });
+
+app.use(unknownEndPoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {

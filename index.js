cf026a56ece3b70d73ade20f8be98e7d9307f57c
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-09 10:37:14 +0200

    Refactor catch block statements to error handle app using error handler middleware - 'errorHandler' function

--- index.js
+++ index.js
@@ -58,20 +58,24 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end();
     })
-    .catch(error => {
-      console.error("Error deleting entry:", error);
-    });
+    .catch(error => next(error));
 });
 
 app.post("/api/persons", (request, response) => {

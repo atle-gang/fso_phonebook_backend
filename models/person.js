c0a2ae97871e4216515d1a4bfa96b5f207156462
Atlegang Sethono <atlegangcode@gmail.com>
2024-10-09 13:32:01 +0200

    Add phone number validation to application that will make sure numbers are of the correct form

--- models/person.js
+++ models/person.js
@@ -19,7 +19,16 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v) && v.replace('-', '').length >= 8;
+      },
+      message: props => `${props.value} is not a valid phone number`
+    },
+    required: [true, 'User phone number is required']
+  }
 });
 
 personSchema.set("toJSON", {
